3d42a4f5396d0fdfc62b2af9ba3caa78e64ad787
Giulia <giu.olivers@gmail.com>
2021-09-21 00:38:52 -0300

    Atualiza script DML

--- scripts/script-dml.sql
+++ scripts/script-dml.sql
@@ -32,7 +32,8 @@ values ('Rua Boa Vista', 8677, 'Ap. 405' , 'São José', 'Rio Branco', 'AC', 'Bras
 ('Rua Santa Maria', 2975, 'Casa 8' , 'Planalto', 'Tailândia', 'PA', 'Brasil', 69948331),
 ('Rua Vinte e Um', 903, 'Anexo 5' , 'São Francisco', 'Criciúma', 'SC', 'Brasil', 57781853),
 ('Rua Primeiro de Maio', 6756, 'Sala 7' , 'São Cristóvão', 'Itacoatiara', 'AM', 'Brasil', 59889557),
-('Rua Dezoito', 1405, 'Sobreloja 8' , 'Santo Antônio', 'Brasília', 'DF', 'Brasil', 90289264);
+('Rua Dezoito', 1405, 'Sobreloja 8' , 'Santo Antônio', 'Brasília', 'DF', 'Brasil', 90289264),
+('Rua Pará', 26, 'Galpão 3' , 'São José', 'Brasília', 'DF', 'Brasil', 48550228);
 
 -- Insert clientes
 
@@ -40,8 +41,9 @@ insert into clientes (nome, sobrenome, idusuario, email, cpf, data_nascimento, i
 values ('Pedro', 'Lima', 46582, 'pedro.lima@gmail.com', 61164469061, '1943-11-29', 1),
 ('Rafael ', 'Fernandes', 85642, 'rafael.fernandes@gmail.com', 23174379350, '1987-08-18', 2),
 ('Lucas', 'Souza', 32168, 'lucas.souza@gmail.com', 25580613644, '1979-05-05',3),
-('Amanda ', 'Alves', 74535, 'amanda.alves@gmail.com', 25580613644, '1954-01-14',4),
-('Ana', 'Cavalcanti', 45367, 'ana.cavalcanti@gmail.com', 14460764156, '1985-08-26',5);
+('Amanda ', 'Alves', 74535, 'amanda.alves@gmail.com', 37833229200, '1954-01-14',4),
+('Ana', 'Cavalcanti', 45367, 'ana.cavalcanti@gmail.com', 14460764156, '1985-08-26',5),
+('João', 'Mendes', 57825, 'joao.mendes@gmail.com', 51139092111, '1971-01-25', 6);
 
 -- Insert data de fabricação
 
@@ -60,3 +62,27 @@ values (30, 1),
 (10, 3),
 (40, 4),
 (100, 5);
+
+-- Exclusão da coluna id_pedidos e adição e atualiazação da coluna id_cliente na tabela pedidos
+
+alter table pedidos drop column id_pedidos;
+alter table pedidos add column id_cliente integer;
+alter table pedidos add constraint id_cliente foreign key (id_cliente) references clientes(id);
+alter table produtos_pedidos add column quantidade integer;
+
+-- Insert pedidos
+
+insert into pedidos (data_pedido, id_cliente)
+values ('2021-08-01', 1),
+('2021-05-22', 2),
+('2021-10-12', 3),
+('2021-07-19', 4),
+('2021-02-09', 5);
+
+insert into produtos_pedidos (id_produtos, id_pedidos, quantidade)
+values (1, 1, 2),
+(2, 1, 1),
+(2, 2, 1),
+(3, 3, 3),
+(4, 4, 2),
+(5, 5, 1);
